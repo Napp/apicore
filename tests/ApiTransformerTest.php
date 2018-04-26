d8412a325cbf1c296e2735b886c54064e6f5d095
Daxi Gudjonsson <dg@napp.dk>
2018-04-26 08:24:35 +0200

    added test for collections in transform output

--- tests/ApiTransformerTest.php
+++ tests/ApiTransformerTest.php
@@ -93,4 +93,80 @@ class ApiTransformerTest extends TestCase
 
         $this->assertEquals($expectedOutput, $transformedOutput);
     }
+
+    public function test_output_transforming_with_collection()
+    {
+        $output = collect();
+        $output->push([
+            'id' => 1,
+            'name' => 'Wayne Industries',
+            'has_access' => 0,
+            'some_additional_field' => 'some_additional_value'
+        ]);
+
+        $output->push([
+            'id' => 2,
+            'name' => 'LexCorp',
+            'has_access' => 1,
+            'some_additional_field' => 'some_additional_value'
+        ]);
+
+        $expectedOutput = [
+            [
+                'id' => 1,
+                'companyName' => 'Wayne Industries',
+                'hasAccess' => false,
+                'some_additional_field' => 'some_additional_value'
+            ],
+            [
+                'id' => 2,
+                'companyName' => 'LexCorp',
+                'hasAccess' => true,
+                'some_additional_field' => 'some_additional_value'
+            ]
+        ];
+
+        $transformedOutput = $this->transformer->transformOutput($output);
+
+        $this->assertEquals($expectedOutput, $transformedOutput);
+    }
+
+    public function test_output_transforming_with_collection_strict_mode()
+    {
+        $reflection = new \ReflectionProperty(\get_class($this->transformer), 'strict');
+        $reflection->setAccessible(true);
+        $reflection->setValue($this->transformer, true);
+
+        $output = collect();
+        $output->push([
+            'id' => 1,
+            'name' => 'Wayne Industries',
+            'has_access' => 0,
+            'some_additional_field' => 'some_additional_value'
+        ]);
+
+        $output->push([
+            'id' => 2,
+            'name' => 'LexCorp',
+            'has_access' => 1,
+            'some_additional_field' => 'some_additional_value'
+        ]);
+
+        $expectedOutput = [
+            [
+                'id' => 1,
+                'companyName' => 'Wayne Industries',
+                'hasAccess' => false,
+            ],
+            [
+                'id' => 2,
+                'companyName' => 'LexCorp',
+                'hasAccess' => true,
+            ]
+        ];
+
+        $transformedOutput = $this->transformer->transformOutput($output);
+
+        $this->assertEquals($expectedOutput, $transformedOutput);
+    }
 }
